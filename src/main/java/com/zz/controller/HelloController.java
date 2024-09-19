0a7ce2cf3f2ae272bc4f2086ff2f68af24d9fd14
zhangzhe <a252013135@163.com>
2024-09-19 17:06:29 +0800

    添加ID请求

--- src/main/java/com/zz/controller/HelloController.java
+++ src/main/java/com/zz/controller/HelloController.java
@@ -2,6 +2,7 @@ package com.zz.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,5 +12,10 @@ public class HelloController {
     @GetMapping("/name")
     public String getName(){
         return this.name;
+    }
+
+    @GetMapping("/hello/{id}")
+    public String getId(@PathVariable("id") String id){
+        return id;
     }
 }
